a8335b105042c93adf86d71034042e5322981f36
gungunbali <gungunbali.hky@gmail.com>
2025-11-21 07:19:55 +0530

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # apsattendance
 # apsattendance
 # apsattendance
+# apsattendance
--- main.py
+++ main.py
@@ -4,12 +4,18 @@ app = Flask(__name__)
 
 # Google Drive project links
 projects = [
-    ("Project 1", "https://drive.google.com/file/d/1VkzeAwn-r2rfkX1MkhUTEdujmWZqXlGX/view?usp=sharing"),
-    ("Project 2", "https://drive.google.com/file/d/1kR-Uy4nLIOgKBttblok9r_s8QyK2gWYD/view?usp=sharing"),
-    ("Project 3", "https://drive.google.com/file/d/1fSnrXAQ2aQNeN8XhCdyfCkEXwLpMgckt/view?usp=sharing"),
-    ("Project 4", "https://drive.google.com/file/d/118CZPVBhCqfY87E_GJJim09vj526XSNA/view?usp=sharing"),
-    ("Project 5", "https://drive.google.com/file/d/14KA-LladQ041wEaI8dLoSoyDJ2m6wTXO/view?usp=sharing"),
-    ("Project 6", "https://drive.google.com/file/d/1LjMGmIiUqbt_rzvfDU1aq0BTdit5CgfM/view?usp=sharing"),
+    ("ann", "https://drive.google.com/file/d/18GTCceLzBvDmP9CFnUUzIhEESxtrNpi4/view?usp=sharing"),
+    ("A*", "https://drive.google.com/file/d/1-Rf6XYg6UNMzgCPohrudqWZSrs9h9Cdg/view?usp=sharing"),
+    ("Candidate Eliminate Algorithm", "https://drive.google.com/file/d/1wirCsSM8Q3gJPGEmdhxuLNekRDyqm_mK/view?usp=sharing"),
+    ("Decision tree", "https://drive.google.com/file/d/1k3GUIMXQCLLEYTi2JxLGI-ZMSOTmmPKP/view?usp=sharing"),
+    ("Decision Tree 2", "https://drive.google.com/file/d/1N9yVlKjKaVG1VLex2HiyyFkqqAIGE3fe/view?usp=sharing"),
+    ("em", "https://drive.google.com/file/d/1KIPY4QF9oeGLijyobHn2BL7WWwI9Xpil/view?usp=sharing"),
+("KMeans", "https://drive.google.com/file/d/10dxf4upFwjq1aS_5SI8FDy6MBPQquEVS/view?usp=sharing"),
+("KNN", "https://drive.google.com/file/d/1mQs1az6L2z6f4hwEvsDLKEK5ujcQdrRk/view?usp=sharing"),
+("multivar", "https://drive.google.com/file/d/1bUBwa6U5a8P0EBvNSR_zjwVCmnBOBdfG/view?usp=sharing"),
+    ("navie","https://drive.google.com/file/d/1qzyD8SRdaoEH77xjcHiRM5irhytJiwCj/view?usp=sharing"),
+    ("svm","https://drive.google.com/file/d/1ozGbjBkXZbYskc4YmVLQ7zArJpQYVSE6/view?usp=sharing"),
+    ("lab1","https://drive.google.com/file/d/1yr4VW4Q4sZXl2ZMFCeKs8tOqoriFv0GU/view?usp=sharing")
 ]
 
 HTML_TEMPLATE = """
